69947c0b62c408edc90d7ee3abb789feaeb312f6
InesBichon <ines.bichon@polytechnique.edu>
2024-11-23 21:27:53 +0100

    Delete chatbot/chat_project directory

--- chatbot/chat_project/chatbox/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.index, name='index'),  # Main chatbox view
-    path('reset/', views.reset, name='reset'),  # Reset view
-]
